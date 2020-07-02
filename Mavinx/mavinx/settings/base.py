859d62e47f04c231a9bd69f47e830a8f83b23403
Mavinxukr <mavinx.office@gmail.com>
2020-07-02 10:43:47 +0300

    freeze

--- Mavinx/mavinx/settings/base.py
+++ Mavinx/mavinx/settings/base.py
@@ -4,14 +4,6 @@ from django.utils.translation import ugettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY','p!m^3jokr&bebebepi@ssa@ujq8uk&vum@majq-@v&t_01y&%m')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

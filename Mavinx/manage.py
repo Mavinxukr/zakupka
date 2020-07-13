5e9dec3717e99abf6caaf2ddeff4c865ec6e602a
Mavinxukr <mavinx.office@gmail.com>
2020-07-13 13:46:43 +0300

    new

--- Mavinx/manage.py
+++ Mavinx/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mavinx.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mavinx.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

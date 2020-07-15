ab9d146cf2db974deebe28a74d692ccd25288630
Mavinxukr <mavinx.office@gmail.com>
2020-07-15 10:19:03 +0300

    nw

--- Mavinx/head/views.py
+++ Mavinx/head/views.py
@@ -1,5 +1,7 @@
+from django.core.paginator import Paginator
 from django.views import View
 
+from .models import Project
 from . shortcuts import render
 
 
@@ -10,7 +12,12 @@ class Index(View):
 
 class Works(View):
     def get(self, request):
-        return render(request,'site/page-header/works.html', context={})
+        projects = Project.objects.all()
+        paginator = Paginator(projects,2)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request,'site/page-header/works.html', {'page_obj': page_obj})
 
 
 class Services(View):

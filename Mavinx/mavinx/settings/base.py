7509388db9a8b57b423459add81d13c7a79bc9ad
Mavinxukr <mavinx.office@gmail.com>
2020-07-16 11:27:03 +0300

    paginate

--- Mavinx/mavinx/settings/base.py
+++ Mavinx/mavinx/settings/base.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'head',
     'django_cleanup',
-    'request'
+    'request',
 ]
 
 INSTALLED_APPS += (
@@ -49,7 +49,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
             ],
         },
     },

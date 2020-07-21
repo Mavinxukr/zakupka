2ba5fab8aec0a1a180d77f555d8989e184949275
Mavinxukr <mavinx.office@gmail.com>
2020-07-21 12:23:56 +0300

    Merge branch 'master-gunicorn' of https://github.com/Mavinxukr/mavinx into master-gunicorn
    # Conflicts:
    #	Mavinx/mavinx/templates/site/head/infrastructure.html

--- Mavinx/head/shortcuts.py
+++ Mavinx/head/shortcuts.py
@@ -1,10 +1,11 @@
+from django.db.models.functions import Lower
 from django.shortcuts import render as main_render
 
 from .models import Area, Contact, Project, Review, Blog
 
 
 def render(request, template, context):
-    context['areas'] =  Area.objects.all().order_by('-translations__name').distinct()
+    context['areas'] =  Area.objects.order_by(Lower('translations__name')).distinct()
     context['contact'] = Contact.objects.first()
     context['reviews'] = Review.objects.all().order_by('-id')
     context['blogs'] = Blog.objects.all()
@@ -13,4 +14,5 @@ def render(request, template, context):
     context['projects_android'] = Project.objects.filter(area__translations__name__contains='Android').order_by('priority').distinct()[:5]
     context['projects_web'] = Project.objects.filter(area__translations__name__contains='Web').order_by('priority').distinct()[:5]
     context['projects_ux_ui'] = Project.objects.filter(area__translations__name__contains='UI/UX').order_by('priority').distinct()[:5]
+    print(context['areas'])
     return main_render(request, template, context)

23271fb3194961f4e607876360bf7294abebfe40
Chepiga Denis <Denclub2013@gmail.com>
2020-08-21 16:17:36 +0300

    50% of header and footer

--- src/Components/shared/Footer/Footer.jsx
+++ src/Components/shared/Footer/Footer.jsx
@@ -2,8 +2,33 @@ import React from 'react';
 import styles from './Footer.scss';
 
 const Footer = () => (
-  <div className={styles.footer}>
-    <h2>this is footer</h2>
+  <div className={styles.container}>
+    <div className={styles.footer}>
+      <ul className={styles.footerMenu}>
+        <li className={styles.link}>Тарифи</li>
+        <li className={styles.link}>Контакти</li>
+        <li className={styles.link}>Політика конфіденційності</li>
+        <li className={styles.link}>Публічна оферта</li>
+        <li className={styles.link}>Регламент</li>
+        <li className={styles.link}>Про майданчик</li>
+        <li className={styles.link}>Навчання</li>
+        <li className={styles.link}>Тренажер аукціонів</li>
+        <li className={styles.link}>Мобільний додаток</li>
+        <li className={styles.link}>Інтеграція з нами</li>
+      </ul>
+      <div className={styles.footerBottom}>
+        <span>&copy;zakupi.prom.ua, 2016-2020</span>
+        <p>Акредитований учасник електронної системи публічних закупівель України ProZorro</p>
+        <ul className={styles.footerLang}>
+          <li className={`${styles.social} ${styles.link}`}>Русский</li>
+          <li className={`${styles.social} ${styles.link}`}>English</li>
+        </ul>
+        <ul className={styles.footerSocial}>
+          <li className={`${styles.facebook} ${styles.link}`}> Facebook</li>
+          <li className={`${styles.youtube} ${styles.link}`}>Youtube</li>
+        </ul>
+      </div>
+    </div>
   </div>
 );
 
--- src/Components/shared/Header/Header.jsx
+++ src/Components/shared/Header/Header.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import styles from './Header.scss';
 
 const Header = () => (
-  <div className={styles.header}>
-    <div className={styles.tab}>
-      <h2>Постачальник</h2>
-    </div>
-    <div className={styles.tab}>
-      <h2>Державний замовник</h2>
-    </div>
-    <div className={styles.tab}>
-      <h2>Комерційний замовник</h2>
-    </div>
-    <div className={styles.tab}>
-      <h2>Учасник аукціонів</h2>
-    </div>
-    <div className={styles.tab}>
-      <h2>Організатор аукціонів</h2>
+  <div className={styles.container}>
+    <div className={styles.header}>
+      <div className={`${styles.tab} ${styles.active}`}>
+        <h2 className={styles.tabTitle}>Постачальник</h2>
+      </div>
+      <div className={styles.tab}>
+        <h2 className={styles.tabTitle}>Державний замовник</h2>
+      </div>
+      <div className={styles.tab}>
+        <h2 className={styles.tabTitle}>Комерційний замовник</h2>
+      </div>
+      <div className={styles.tab}>
+        <h2 className={styles.tabTitle}>Учасник аукціонів</h2>
+      </div>
+      <div className={styles.tab}>
+        <h2 className={styles.tabTitle}>Організатор аукціонів</h2>
+      </div>
     </div>
   </div>
 );

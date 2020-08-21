8f7bb6f4ffa6ed5331039133439d99ac4d56ea69
Chepiga Denis <Denclub2013@gmail.com>
2020-08-21 13:12:40 +0300

    Start layout header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Components/shared/Header/Header';
+import Footer from './Components/shared/Footer/Footer';
+
 import './index.scss';
 
 const App = () => (
   <div className="App">
     <Header />
     <h1>Start zakupka</h1>
+    <Footer />
   </div>
 );
 
--- src/Components/shared/Header/Header.jsx
+++ src/Components/shared/Header/Header.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import styles from './Header.scss';
 
 const Header = () => (
-  <div className={styles.header}>Header</div>
+  <div className={styles.header}>
+    <div className={styles.tab}>
+      <h2>Постачальник</h2>
+    </div>
+    <div className={styles.tab}>
+      <h2>Державний замовник</h2>
+    </div>
+    <div className={styles.tab}>
+      <h2>Комерційний замовник</h2>
+    </div>
+    <div className={styles.tab}>
+      <h2>Учасник аукціонів</h2>
+    </div>
+    <div className={styles.tab}>
+      <h2>Організатор аукціонів</h2>
+    </div>
+  </div>
 );
 
 export default Header;

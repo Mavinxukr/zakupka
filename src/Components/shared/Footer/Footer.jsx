3b97c311d436f632febca7c1490413be1bec6e57
Chepiga Denis <Denclub2013@gmail.com>
2020-08-21 17:16:36 +0300

    60% header and footer

--- src/Components/shared/Footer/Footer.jsx
+++ src/Components/shared/Footer/Footer.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import styles from './Footer.scss';
 
 const Footer = () => (
-  <div className={styles.container}>
-    <div className={styles.footer}>
-      <ul className={styles.footerMenu}>
-        <li className={styles.link}>Тарифи</li>
-        <li className={styles.link}>Контакти</li>
-        <li className={styles.link}>Політика конфіденційності</li>
-        <li className={styles.link}>Публічна оферта</li>
-        <li className={styles.link}>Регламент</li>
-        <li className={styles.link}>Про майданчик</li>
-        <li className={styles.link}>Навчання</li>
-        <li className={styles.link}>Тренажер аукціонів</li>
-        <li className={styles.link}>Мобільний додаток</li>
-        <li className={styles.link}>Інтеграція з нами</li>
-      </ul>
+  <div className={styles.footer}>
+    <div className={styles.footerContainer}>
+      <div className={styles.container}>
+        <ul className={styles.footerMenu}>
+          <li className={styles.link}>Тарифи</li>
+          <li className={styles.link}>Контакти</li>
+          <li className={styles.link}>Політика конфіденційності</li>
+          <li className={styles.link}>Публічна оферта</li>
+          <li className={styles.link}>Регламент</li>
+          <li className={styles.link}>Про майданчик</li>
+          <li className={styles.link}>Навчання</li>
+          <li className={styles.link}>Тренажер аукціонів</li>
+          <li className={styles.link}>Мобільний додаток</li>
+          <li className={styles.link}>Інтеграція з нами</li>
+        </ul>
+      </div>
+    </div>
+    <div className={styles.container}>
       <div className={styles.footerBottom}>
         <span>&copy;zakupi.prom.ua, 2016-2020</span>
         <p>Акредитований учасник електронної системи публічних закупівель України ProZorro</p>

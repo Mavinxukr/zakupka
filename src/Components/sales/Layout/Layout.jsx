8f0bb8e66041e26f5d34485ed95b1edb646d70b7
Chepiga Denis <Denclub2013@gmail.com>
2020-09-01 12:49:52 +0300

    next version of project

--- src/Components/sales/Layout/Layout.jsx
+++ src/Components/sales/Layout/Layout.jsx
@@ -3,6 +3,8 @@ import cx from 'classnames';
 import { Tabs, useTabState, usePanelState } from '@bumaga/tabs';
 import styles from './Layout.scss';
 import Tab1 from './Tab1/Tab1';
+import Tab2 from './Tab2/Tab2';
+import Tab3 from './Tab3/Tab3';
 import IconAllergies from '../../../static/svg/briefcase-solid.svg';
 import IconList from '../../../static/svg/list.svg';
 import IconBriefcase from '../../../static/svg/allergies.svg';
@@ -42,8 +44,12 @@ const Layout = () => (
       <Panel>
         <Tab1 />
       </Panel>
-      <Panel><p>Panel 2</p></Panel>
-      <Panel><p>panel 3</p></Panel>
+      <Panel>
+        <Tab2 />
+      </Panel>
+      <Panel>
+        <Tab3 />
+      </Panel>
     </Tabs>
   </div>
 );

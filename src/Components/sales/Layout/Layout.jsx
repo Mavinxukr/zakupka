9eec76e500ea61574ce2616158b1d8e011b9382b
SamborVadym <samborvadym@gmail.com>
2020-09-09 08:20:10 +0300

    fixed

--- src/Components/sales/Layout/Layout.jsx
+++ src/Components/sales/Layout/Layout.jsx
@@ -1,9 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import cx from 'classnames';
 import { Tabs, useTabState, usePanelState } from '@bumaga/tabs';
-import { useDispatch, useSelector } from 'react-redux';
-import { tendersDataSelector, tendersDataReceivedSelector } from '../../../utils/selectors';
-import { getTenders } from '../../../redux/actions/tenders';
 import styles from './Layout.scss';
 import Tab1 from './Tab1/Tab1';
 import Tab2 from './Tab2/Tab2';
@@ -27,48 +24,34 @@ const Panel = ({ children }) => {
   return isActive ? <div>{children}</div> : null;
 };
 
-const Layout = () => {
-  const dispatch = useDispatch();
-  const tenders = useSelector(tendersDataSelector);
-  const isDataReceived = useSelector(tendersDataReceivedSelector);
-
-  useEffect(() => {
-    dispatch(getTenders({}));
-  }, []);
-
-  if (!isDataReceived) {
-    return <div />;
-  }
-
-  return (
-    <div className={styles.flexContainer}>
-      <Tabs>
-        <div className={styles.buttonTabs}>
-          <Tab>
-            <IconBriefcase className={styles.icon} />
-            <span>Всі закупівлі</span>
-          </Tab>
-          <Tab>
-            <IconList className={styles.icon} />
-            <span>Плани закупівель</span>
-          </Tab>
-          <Tab>
-            <IconAllergies className={styles.icon} />
-            <span>Підписані контракти</span>
-          </Tab>
-        </div>
-        <Panel>
-          <Tab1 tenders={tenders} />
-        </Panel>
-        <Panel>
-          <Tab2 />
-        </Panel>
-        <Panel>
-          <Tab3 />
-        </Panel>
-      </Tabs>
-    </div>
-  );
-};
+const Layout = () => (
+  <div className={styles.flexContainer}>
+    <Tabs>
+      <div className={styles.buttonTabs}>
+        <Tab>
+          <IconBriefcase className={styles.icon} />
+          <span>Всі закупівлі</span>
+        </Tab>
+        <Tab>
+          <IconList className={styles.icon} />
+          <span>Плани закупівель</span>
+        </Tab>
+        <Tab>
+          <IconAllergies className={styles.icon} />
+          <span>Підписані контракти</span>
+        </Tab>
+      </div>
+      <Panel>
+        <Tab1 />
+      </Panel>
+      <Panel>
+        <Tab2 />
+      </Panel>
+      <Panel>
+        <Tab3 />
+      </Panel>
+    </Tabs>
+  </div>
+);
 
 export default Layout;
--- src/Components/sales/Layout/Tab1/Tab1.jsx
+++ src/Components/sales/Layout/Tab1/Tab1.jsx
@@ -1,90 +1,106 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import IconStart from '../../../../static/svg/star.svg';
 import styles from './Tab1.scss';
 import Filter from '../Filters/Filters';
+import { tendersDataReceivedSelector, tendersDataSelector } from '../../../../utils/selectors';
+import { getTenders } from '../../../../redux/actions/tenders';
 
-const Tab1 = ({ tenders }) => (
-  <div className={styles.main}>
-    <div className={styles.pills}>
-      <button type="button">все</button>
-      <button type="button">избранные</button>
-    </div>
-    <div className={styles.bgWhite}>
-      <Filter className={styles.top} />
-      <h1 className={styles.titleFilter}>Державні закупівлі
-      </h1>
-      <div className={styles.titleContainer}>
-        <p className={styles.leftBlock}>Предмет закупівлі</p>
-        <div className={styles.middleBlock}>
-          <p> Очікувана вартість </p>
-          <p>Важливі дати</p>
-        </div>
-        <p>Статус</p>
+const Tab1 = () => {
+  const dispatch = useDispatch();
+  const tenders = useSelector(tendersDataSelector);
+  const isDataReceived = useSelector(tendersDataReceivedSelector);
+
+  useEffect(() => {
+    dispatch(getTenders({}));
+  }, []);
+
+  if (!isDataReceived) {
+    return <div />;
+  }
+  return (
+    <div className={styles.main}>
+      <div className={styles.pills}>
+        <button type="button">все</button>
+        <button type="button">избранные</button>
       </div>
-    </div>
-    {tenders.data.map((item) => {
-      const tender = JSON.parse(item.tender);
+      <div className={styles.bgWhite}>
+        <Filter className={styles.top} />
+        <h1 className={styles.titleFilter}>Державні закупівлі
+        </h1>
+        <div className={styles.titleContainer}>
+          <p className={styles.leftBlock}>Предмет закупівлі</p>
+          <div className={styles.middleBlock}>
+            <p> Очікувана вартість </p>
+            <p>Важливі дати</p>
+          </div>
+          <p>Статус</p>
+        </div>
+      </div>
+      {tenders.data.map((item) => {
+        const tender = JSON.parse(item.tender);
 
-      return (
-        <div className={styles.bgWhite} key={item.id}>
-          <div className={styles.titleContainer}>
-            <div className={styles.leftBlock}>
-              <a href="/">{tender.title}</a>
-              <span className={styles.contentText}>
-                {item.customer_legalName}
-              </span>
-              <span className={styles.contentText}>
-                О компании
-              </span>
-              <span className={styles.contentText}>
-                {tender.tenderID}
-              </span>
-            </div>
-            <div className={styles.middleBlock}>
-              <div className={styles.sumBlock}>
-                <span className={styles.sum}>{item.bet_price} ₴</span>
-                <span className={styles.data}>c НДС</span>
-              </div>
-              <div>
+        return (
+          <div className={styles.bgWhite} key={item.id}>
+            <div className={styles.titleContainer}>
+              <div className={styles.leftBlock}>
+                <a href="/">{tender.title}</a>
                 <span className={styles.contentText}>
-                  Объявлена::
-                </span>
-                <span className={styles.data}>
-                  31 серп., 11:19
+                  {item.customer_legalName}
                 </span>
                 <span className={styles.contentText}>
-                  Завершение периода уточнений:
-                </span>
-                <span className={styles.data}>
-                  31 серп., 11:45
+                  О компании
                 </span>
                 <span className={styles.contentText}>
-                  Прием предложений:
-                </span>
-                <span className={styles.data}>
-                  31 серп., 11:19 – 31 серп., 11:55
+                  {tender.tenderID}
                 </span>
               </div>
-            </div>
-            <div className={styles.rightBLock}>
-              <span className={styles.period}>
-                Період уточнень
-              </span>
-              <span className={styles.data}>
-                Осталось: --
-              </span>
-              <div className={styles.iconBlock}>
-                <IconStart className={styles.iconStar} />
-                <span className={styles.data}>
-                  Добавить в избранное
+              <div className={styles.middleBlock}>
+                <div className={styles.sumBlock}>
+                  <span className={styles.sum}>{item.bet_price} ₴</span>
+                  <span className={styles.data}>c НДС</span>
+                </div>
+                <div>
+                  <span className={styles.contentText}>
+                    Объявлена::
+                  </span>
+                  <span className={styles.data}>
+                    31 серп., 11:19
+                  </span>
+                  <span className={styles.contentText}>
+                    Завершение периода уточнений:
+                  </span>
+                  <span className={styles.data}>
+                    31 серп., 11:45
+                  </span>
+                  <span className={styles.contentText}>
+                    Прием предложений:
+                  </span>
+                  <span className={styles.data}>
+                    31 серп., 11:19 – 31 серп., 11:55
+                  </span>
+                </div>
+              </div>
+              <div className={styles.rightBLock}>
+                <span className={styles.period}>
+                  Період уточнень
                 </span>
+                <span className={styles.data}>
+                  Осталось: --
+                </span>
+                <div className={styles.iconBlock}>
+                  <IconStart className={styles.iconStar} />
+                  <span className={styles.data}>
+                    Добавить в избранное
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      );
-    })}
-  </div>
-);
+        );
+      })}
+    </div>
+  );
+};
 
 export default Tab1;
